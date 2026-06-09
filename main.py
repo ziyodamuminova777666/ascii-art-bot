fd9c6c49bf4bdff1c915abecb1b24c870559722d
ziyodamuminova777666 <ziyodamuminova98@gmail.com>
2026-06-10 01:35:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -17,19 +17,14 @@ async def ascii_art(update: Update, context: ContextTypes.DEFAULT_TYPE):
 ＼   ／   (･ω･ )
 ((＼／     ⊂ )
 """
-
     await update.message.reply_text(art)
-
 
 def main():
     app = Application.builder().token(TOKEN).build()
-
     app.add_handler(
         MessageHandler(filters.TEXT & ~filters.COMMAND, ascii_art)
     )
-
     app.run_polling()
-
 
 if __name__ == "__main__":
     main()
