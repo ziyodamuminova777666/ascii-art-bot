be27de852d62d929739af50112e87e570ffa4e16
ziyodamuminova777666 <ziyodamuminova98@gmail.com>
2026-06-10 00:48:20 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from telegram import Update
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def ascii_art(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+
+    art = f"""
+       ／＼
+     ／   ＼ ))
+    ／     ／
+   ／     ／      {text}
+  ／     ／
+／     ／＼ ∧_∧
+＼   ／   (･ω･ )
+((＼／     ⊂ )
+"""
+
+    await update.message.reply_text(art)
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, ascii_art))
+
+app.run_polling()
